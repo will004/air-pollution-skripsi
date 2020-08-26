0272555d165999c835a82980d7509e54fd6a7fcd
will004 <williamalfredo85@gmail.com>
2020-08-26 08:27:35 +0700

    fix formatting

--- process.py
+++ process.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class Simulation:
     def __init__(self, v, D, a, b, t0, t1, initial_condition, dt, dx=0.5):
         # Initialize variables
@@ -23,9 +24,9 @@ class Simulation:
         # Concentration as C
         self.C = np.empty((self.Nt+1, self.Nx+1))
         # Initial condition: C(x,0) = initial_condition
-        self.C[0,:] = initial_condition
+        self.C[0, :] = initial_condition
         # Boundary condition: C(0,t) = 1
-        self.C[:,0] = 1
+        self.C[:, 0] = 1
         # ===============================================================
         # Simulate
         self.idx_t = 0
@@ -36,20 +37,22 @@ class Simulation:
             for j in self.x:
                 # boundary condition, Cx(inf,t) = 0 -> C_{j+1}^n = C_{j}^n
                 if self.idx_x+1 == len(self.x):
-                    self.C[self.idx_t+1, self.idx_x] = self.FTBSCS(self.C, self.idx_t, self.idx_x, self.alpha, self.beta, bound=True)
+                    self.C[self.idx_t+1, self.idx_x] = self.FTBSCS(
+                        self.C, self.idx_t, self.idx_x, self.alpha, self.beta, bound=True)
                     break
-                self.C[self.idx_t+1, self.idx_x] = self.FTBSCS(self.C, self.idx_t, self.idx_x, self.alpha, self.beta)
+                self.C[self.idx_t+1, self.idx_x] = self.FTBSCS(
+                    self.C, self.idx_t, self.idx_x, self.alpha, self.beta)
 
                 self.idx_x += 1
             self.idx_t += 1
         # ===============================================================
         self.simulationResult = {
-            'C':self.C,
-            'x':self.x,
-            't':self.t,
-            'v':v,
-            'D':D
-            }
+            'C': self.C,
+            'x': self.x,
+            't': self.t,
+            'v': v,
+            'D': D
+        }
 
     def FTBSCS(self, C, idx_t, idx_x, alpha, beta, bound=False):
         if bound == True:
